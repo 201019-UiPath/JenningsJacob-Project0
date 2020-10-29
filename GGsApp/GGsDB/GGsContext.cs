7620bc507d199c8a800879d95a8a5e017eea5383
Jacob <jacobjennings510@gmail.com>
2020-10-29 14:00:05 -0700

    Set up connection to DB

---
+++ GGsApp/GGsDB/GGsContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using GGsDB.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace GGsDB
+{
+    public class GGsContext : DbContext
+    {
+        public DbSet<Customer> Customers {get; set;}
+        public DbSet<Manager> Managers {get; set;}
+        public DbSet<Inventory> Inventories {get; set;}
+        public DbSet<Location> Locations {get; set;}
+        public DbSet<Order> Orders {get; set;}
+        public DbSet<GameConsole> GameConsoles {get; set;}
+        public DbSet<VideoGame> VideoGames {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+                var connectionString = configuration.GetConnectionString("GGsDB");
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+    }
+}
