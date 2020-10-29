6e69fe66e85c60fa438e1936e0c9e49593bccf86
Jacob <jacobjennings510@gmail.com>
2020-10-29 13:59:39 -0700

    Manager now inherits Person

--- GGsApp/GGsDB/Models/Manager.cs
+++ GGsApp/GGsDB/Models/Manager.cs
@@ -1,9 +1,8 @@
 namespace GGsDB.Models
 {
-    public class Manager
+    public class Manager : Person
     {
         public int LocationId{get; set;}
         public Location StoreAddress{get; set;}
-        public string Email{get; set;}
     }
 }
